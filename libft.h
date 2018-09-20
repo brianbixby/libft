c606db9260e697f39ac8be8a65a8c68541a7b34e
Brian Bixby <brianbixby0@gmail.com>
2018-09-20 03:45:22 -0700

    removed list function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bbixby <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 17:39:19 by bbixby            #+#    #+#             */
-/*   Updated: 2018/09/18 23:12:57 by bbixby           ###   ########.fr       */
+/*   Updated: 2018/09/20 03:44:32 by bbixby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,5 @@ void				ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
 #endif
